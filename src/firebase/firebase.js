8905f11b9e739dff922600157b466b8e1e8f5e74
elit-altum <elit.altum@gmail.com>
2019-12-27 14:15:44 +0530

    Firebase amends

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -2,6 +2,7 @@ import * as firebase from 'firebase';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
+    
     apiKey: process.env.FIREBASE_API_KEY,
     authDomain: process.env.FIREBASE_AUTH_DOMAIN,
     databaseURL: process.env.FIREBASE_DATABASE_URL,
