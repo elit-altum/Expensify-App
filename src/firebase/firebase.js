3a4b5055f7b6aefa6f4a22909f26d1647845655f
elit-altum <elit.altum@gmail.com>
2019-12-27 15:25:56 +0530

    Changed env variable parser

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -2,7 +2,6 @@ import * as firebase from 'firebase';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    
     apiKey: process.env.FIREBASE_API_KEY,
     authDomain: process.env.FIREBASE_AUTH_DOMAIN,
     databaseURL: process.env.FIREBASE_DATABASE_URL,
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,7 @@
 const path = require('path');
+const result = require('dotenv').config({ path: '.env.development'});
+
+const webpack = require('webpack');
 
 //Plugin to extract particular extension files as seperate files in webpack
 //Here we use it for CSS/SCSS
@@ -43,6 +46,16 @@ module.exports = (env) => {
     },
     plugins: [  //Plugins used for bundling
       CSSExtract,
+      new webpack.DefinePlugin({
+        'process.env.FIREBASE_API_KEY': JSON.stringify(process.env.FIREBASE_API_KEY),
+        'process.env.FIREBASE_AUTH_DOMAIN': JSON.stringify(process.env.FIREBASE_AUTH_DOMAIN),
+        'process.env.FIREBASE_DATABASE_URL': JSON.stringify(process.env.FIREBASE_DATABASE_URL),
+        'process.env.FIREBASE_PROJECT_ID': JSON.stringify(process.env.FIREBASE_PROJECT_ID),
+        'process.env.FIREBASE_STORAGE_BUCKET': JSON.stringify(process.env.FIREBASE_STORAGE_BUCKET),
+        'process.env.FIREBASE_MESSAGING_SENDER_ID': JSON.stringify(process.env.FIREBASE_MESSAGING_SENDER_ID),
+        'process.env.FIREBASE_APP_ID': JSON.stringify(process.env.FIREBASE_APP_ID),
+        'process.env.FIREBASE_MEASUREMENT_ID': JSON.stringify(process.env.FIREBASE_MEASUREMENT_ID)
+      })
     ],
     devtool: isProduction ? 'source-map' : 'inline-source-map',
     devServer: {
